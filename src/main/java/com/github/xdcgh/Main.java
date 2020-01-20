ce5fe50a137a7363331a3f2e5bfee571566506e4
xdc <1434412784@qq.com>
2020-01-20 14:37:34 +0800

    初步连接好数据库的读写两个Links表
    可以实现将爬取的链接存入LINKS_TO_BE_PROCESSED
    处理过的链接存入LINKS_ALREADY_PROCESSED
    也实现了断点续传的功能。

--- src/main/java/com/github/xdcgh/Main.java
+++ src/main/java/com/github/xdcgh/Main.java
@@ -11,39 +11,80 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 待处理的连接池
-        List<String> linkPool = new ArrayList<>();
-        // 已经处理的连接池
-        Set<String> processedLinks = new HashSet<>();
-        linkPool.add("https://sina.cn");
+    public static void main(String[] args) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:C:/Users/14344/IdeaProjects/xiedaimala-crawler/news", "root", "1234");
 
         while (true) {
+            // 待处理的连接池
+            // 从数据库加载即将处理的链接代码
+            List<String> linkPool = loadUrlsFromDatabase(connection, "select link from LINKS_TO_BE_PROCESSED");
+
             if (linkPool.isEmpty()) {
                 break;
             }
-            // ArrayList从尾部删除更有效率
+
+
+            // 从待处理池子中捞一个来处理
+            // 处理完后从池子（包括数据库）中删除
             String link = linkPool.remove(linkPool.size() - 1);
-            if (processedLinks.contains(link)) {
+            try (PreparedStatement statement = connection.prepareStatement("DELETE FROM LINKS_TO_BE_PROCESSED WHERE LINK = ?")) {
+                statement.setString(1, link);
+                statement.executeUpdate();
+            }
+
+            // 询问数据库，当前链接是否被处理过了
+            Boolean flag = false;
+            try (PreparedStatement statement = connection.prepareStatement("SELECT LINK FROM LINKS_ALREADY_PROCESSED WHERE LINK = ?")) {
+                statement.setString(1,link);
+                ResultSet resultSet = statement.executeQuery();
+                while (resultSet.next()) {
+                    flag = true;
+                }
+            }
+
+            if (flag) {
                 continue;
             }
             // 这是我们感兴趣的链接
             if (isInterestingLink(link)) {
                 Document doc = httpGetAndParseHtml(link);
                 // 把doc 页面内所有的<a> 标签，链接（href）仍进连接池
-                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
+                for (Element aTag : doc.select("a")) {
+                    String href = aTag.attr("href");
+                    try (PreparedStatement statement = connection.prepareStatement("INSERT INTO LINKS_TO_BE_PROCESSED (LINK) VALUES (?)")) {
+                        statement.setString(1, href);
+                        statement.executeUpdate();
+                    }
+                }
                 // 假如这是一个新闻的详情页面，就存入数据库，否则，就什么都不做
                 storeIntoDatabaseIfItIsNewsPage(doc);
-                // 把以处理的链接加入已处理集合中
-                processedLinks.add(link);
+
+                try (PreparedStatement statement = connection.prepareStatement("INSERT INTO LINKS_ALREADY_PROCESSED (LINK) VALUES (?)")) {
+                    statement.setString(1, link);
+                    statement.executeUpdate();
+                }
             }
         }
+    }
+
+    private static List<String> loadUrlsFromDatabase(Connection connection, String sql) throws SQLException {
+        List<String> results = new ArrayList<>();
+
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            ResultSet resultSet = statement.executeQuery();
+            while (resultSet.next()) {
+                results.add(resultSet.getString(1));
+            }
+        }
+
+        return results;
     }
 
     private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
@@ -83,7 +124,7 @@ public class Main {
     }
 
     private static boolean isInterestingLink(String link) {
-        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link) && !link.contains("\\");
     }
 
     private static boolean isIndexPage(String link) {
