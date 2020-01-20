14aa2eced313e024a61861b51005a08619bd65b8
xdcgh <44773625+xdcgh@users.noreply.github.com>
2020-01-20 21:24:29 +0800

    实现基本数据库的操作、爬虫和存储功能
    * 设计并实现基本的算法
    
    这个提交中，我们实现了基本的爬虫算法。
    虽然它还非常原始，但是我们想要小步提交。
    
    * 修复CI风格检查错误
    
    删除闲置的包
    
    * 引入SpotBugs插件，并优化Checkstyle插件。
    
    * 初步连接好数据库的读写两个Links表
    
    可以实现将爬取的链接存入LINKS_TO_BE_PROCESSED
    处理过的链接存入LINKS_ALREADY_PROCESSED
    也实现了断点续传的功能。
    
    * 暂且重构完毕
    
    目前方法都进行了重构，提高代码的可阅读性；
    
    pom 加入FindBugs Annotations 的依赖，
    方便注释掉不用的bug提醒。
    
    * 安装flyway插件，并不把db文件加入git
    
    * 基本完成数据库的操作，实现爬虫和存储功能

--- src/main/java/com/github/xdcgh/Main.java
+++ src/main/java/com/github/xdcgh/Main.java
@@ -1,5 +1,6 @@
 package com.github.xdcgh;
 
+import edu.umd.cs.findbugs.annotations.SuppressFBWarnings;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,49 +12,122 @@ import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 import java.io.IOException;
+import java.sql.*;
 import java.util.ArrayList;
-import java.util.HashSet;
-import java.util.List;
-import java.util.Set;
+import java.util.stream.Collectors;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        // 待处理的连接池
-        List<String> linkPool = new ArrayList<>();
-        // 已经处理的连接池
-        Set<String> processedLinks = new HashSet<>();
-        linkPool.add("https://sina.cn");
+    private static final String USER_NAME = "root";
+    private static final String PASSWORD = "1234";
 
-        while (true) {
-            if (linkPool.isEmpty()) {
-                break;
-            }
-            // ArrayList从尾部删除更有效率
-            String link = linkPool.remove(linkPool.size() - 1);
-            if (processedLinks.contains(link)) {
+    @SuppressFBWarnings("DMI_CONSTANT_DB_PASSWORD")
+    public static void main(String[] args) throws IOException, SQLException {
+        Connection connection = DriverManager.getConnection("jdbc:h2:file:C:/Users/14344/IdeaProjects/xiedaimala-crawler/news", USER_NAME, PASSWORD);
+
+        String link;
+
+        // 从数据库中加载下一个链接，如果能加载到，则进行循环
+        while ((link = getNextLinkThenDelete(connection)) != null) {
+            // 询问数据库，当前链接是否被处理过了
+            if (isLinkProcessed(connection, link)) {
                 continue;
             }
-            // 这是我们感兴趣的链接
+
             if (isInterestingLink(link)) {
+                System.out.println(link);
+
                 Document doc = httpGetAndParseHtml(link);
-                // 把doc 页面内所有的<a> 标签，链接（href）仍进连接池
-                doc.select("a").stream().map(aTag -> aTag.attr("href")).forEach(linkPool::add);
-                // 假如这是一个新闻的详情页面，就存入数据库，否则，就什么都不做
-                storeIntoDatabaseIfItIsNewsPage(doc);
-                // 把以处理的链接加入已处理集合中
-                processedLinks.add(link);
+
+                parseUrlsFromPageAndStoreIntoDatabase(connection, doc);
+
+                storeIntoDatabaseIfItIsNewsPage(connection, doc, link);
+
+                updateDatabase(connection, link, "INSERT INTO LINKS_ALREADY_PROCESSED (LINK) VALUES (?)");
             }
         }
     }
 
-    private static void storeIntoDatabaseIfItIsNewsPage(Document doc) {
+    private static String getNextLinkThenDelete(Connection connection) throws SQLException {
+        String link = getNextLink(connection, "select link from LINKS_TO_BE_PROCESSED");
+
+        if (link != null) {
+            updateDatabase(connection, link, "DELETE FROM LINKS_TO_BE_PROCESSED WHERE LINK = ?");
+        }
+
+        return link;
+    }
+
+    // 把doc 页面内所有的<a> 标签，链接（href）仍进连接池
+    private static void parseUrlsFromPageAndStoreIntoDatabase(Connection connection, Document doc) throws SQLException {
+        for (Element aTag : doc.select("a")) {
+            String href = aTag.attr("href");
+
+            if (href.startsWith("//")) {
+                href = "https:" + href;
+            }
+
+            //过滤有问题的链接
+            if (!href.toLowerCase().startsWith("javascript")
+                    || !href.contains("\\/")
+                    || !href.contains("#")
+                    || !href.contains(" ")) {
+                updateDatabase(connection, href, "INSERT INTO LINKS_TO_BE_PROCESSED (LINK) VALUES (?)");
+            }
+
+        }
+    }
+
+    private static Boolean isLinkProcessed(Connection connection, String link) throws SQLException {
+        ResultSet resultSet = null;
+        try (PreparedStatement statement = connection.prepareStatement("SELECT LINK FROM LINKS_ALREADY_PROCESSED WHERE LINK = ?")) {
+            statement.setString(1, link);
+            resultSet = statement.executeQuery();
+            if (resultSet.next()) {
+                return true;
+            }
+        } finally {
+            if (resultSet != null) {
+                resultSet.close();
+            }
+        }
+        return false;
+    }
+
+    private static void updateDatabase(Connection connection, String link, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql)) {
+            statement.setString(1, link);
+            statement.executeUpdate();
+        }
+    }
+
+    private static String getNextLink(Connection connection, String sql) throws SQLException {
+        try (PreparedStatement statement = connection.prepareStatement(sql); ResultSet resultSet = statement.executeQuery()) {
+            if (resultSet.next()) {
+                return resultSet.getString(1);
+            }
+        }
+
+        return null;
+    }
+
+    // 假如这是一个新闻的详情页面，就存入数据库，否则，就什么都不做
+    private static void storeIntoDatabaseIfItIsNewsPage(Connection connection, Document doc, String link) throws SQLException {
         ArrayList<Element> articleTags = doc.select("article");
         if (!articleTags.isEmpty()) {
             for (Element articleTag :
                     articleTags) {
                 String title = articleTag.child(0).text();
-                System.out.println(title);
-            }
+                String content = articleTag.select("p").stream().map(Element::text).collect(Collectors.joining("\n"));
+
+                System.out.println(title);      // 测试用
+
+                try (PreparedStatement statement = connection.prepareStatement("INSERT INTO NEWS (TITLE, CONTENT, URL, CREATED_AT, MODIFIED_AT) VALUES (?, ?, ?, NOW(), NOW())")) {
+                    statement.setString(1, title);
+                    statement.setString(2, content);
+                    statement.setString(3, link);
+
+                    statement.executeUpdate();
+                }
         }
     }
 
@@ -61,19 +135,12 @@ public class Main {
         // 这是我们感兴趣的，目前我们只处理新浪站内的连接
         CloseableHttpClient httpclient = HttpClients.createDefault();
 
-        // 特殊链接，特殊处理
-        if (link.startsWith("//")) {
-            link = "https:" + link;
-            System.out.println(link);
-        }
-
         // 获取链接请求
         HttpGet httpGet = new HttpGet(link);
         httpGet.addHeader("User-Agent",
                 "Mozilla/5.0 (Windows NT 10.0; Win64; x64; rv:72.0) Gecko/20100101 Firefox/72.0");
 
         try (CloseableHttpResponse response1 = httpclient.execute(httpGet)) {
-            System.out.println(response1.getStatusLine());
             HttpEntity entity1 = response1.getEntity();
 
             String html = EntityUtils.toString(entity1);
@@ -83,7 +150,7 @@ public class Main {
     }
 
     private static boolean isInterestingLink(String link) {
-        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link);
+        return (isNewsPage(link) || isIndexPage(link)) && isNotLoginPage(link) && !link.contains("\\");
     }
 
     private static boolean isIndexPage(String link) {
