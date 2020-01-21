7830c47fb8924d548c94c24d7890f11d3c68b0d0
xdc <1434412784@qq.com>
2020-01-21 22:18:50 +0800

    依照课程，完成我们的ElasticSearch的新闻搜索功能！

--- src/main/java/com/github/xdcgh/ElasticSearchDataGenerator.java
+++ src/main/java/com/github/xdcgh/ElasticSearchDataGenerator.java
@@ -1,4 +1,75 @@
 package com.github.xdcgh;
 
+import org.apache.http.HttpHost;
+import org.apache.ibatis.io.Resources;
+import org.apache.ibatis.session.SqlSession;
+import org.apache.ibatis.session.SqlSessionFactory;
+import org.apache.ibatis.session.SqlSessionFactoryBuilder;
+import org.elasticsearch.action.bulk.BulkRequest;
+import org.elasticsearch.action.bulk.BulkResponse;
+import org.elasticsearch.action.index.IndexRequest;
+import org.elasticsearch.action.index.IndexResponse;
+import org.elasticsearch.client.RequestOptions;
+import org.elasticsearch.client.RestClient;
+import org.elasticsearch.client.RestHighLevelClient;
+import org.elasticsearch.common.xcontent.XContentType;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
 public class ElasticSearchDataGenerator {
+    public static void main(String[] args) {
+        SqlSessionFactory sqlSessionFactory;
+        try {
+            String resource = "db/mybatis/config.xml";
+            InputStream inputStream = Resources.getResourceAsStream(resource);
+            sqlSessionFactory = new SqlSessionFactoryBuilder().build(inputStream);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        List<News> newsFromMySQL = getNewsFromMySQL(sqlSessionFactory);
+
+        for (int i = 0; i < 4; i++) {
+            new Thread(()->writeSingleThread(newsFromMySQL)).start();
+        }
+    }
+
+    private static void writeSingleThread(List<News> newsFromMySQL) {
+        try (RestHighLevelClient client = new RestHighLevelClient(RestClient.builder(new HttpHost("localhost", 9200, "http")))) {
+            for (int i = 0; i < 1000; i++) {
+                BulkRequest bulkRequest = new BulkRequest();
+                for (News news :
+                        newsFromMySQL) {
+                    IndexRequest request = new IndexRequest("news");
+
+                    Map<String, Object> data = new HashMap<>();
+                    data.put("content", news.getContent());
+                    data.put("url", news.getUrl());
+                    data.put("title", news.getTitle());
+                    data.put("createdAt", news.getCreatedAt());
+                    data.put("modifiedAt", news.getModifiedAt());
+
+                    request.source(data, XContentType.JSON);
+
+                    bulkRequest.add(request);
+//                    IndexResponse response = client.index(request, RequestOptions.DEFAULT);
+                }
+                BulkResponse bulkResponse = client.bulk(bulkRequest, RequestOptions.DEFAULT);
+                System.out.println("Current thread: " + Thread.currentThread().getName()
+                        + " finishes " + i + ":" + bulkResponse.status().getStatus());
+            }
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static List<News> getNewsFromMySQL(SqlSessionFactory sqlSessionFactory) {
+        try (SqlSession session = sqlSessionFactory.openSession()) {
+            return session.selectList("com.github.xdcgh.MockMapper.selectNews");
+        }
+    }
 }
